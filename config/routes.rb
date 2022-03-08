7deaf13e3f5a20261d73f399e7bf640a7584c274
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2022-03-08 09:20:13 +0000

    [Start]3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists'=> 'lists#create' #書き換え忘れるとエラー起こす
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists/new'
+  get '/top' => "homes#top"
+  post 'lists'=> 'lists#create' #書き換え忘れるとエラー起こす
+  get 'lists' => 'lists#index'
+  #get 'lists/show'
+  #get 'lists/edit'
+  
+ 
 end
