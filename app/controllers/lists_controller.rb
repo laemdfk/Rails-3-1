740790d569efee93bddfeef2e4acbb54a7726fb0
EC2 Default User <ec2-user@ip-172-31-2-160.ec2.internal>
2022-03-08 17:43:23 +0000

    [finish]3-7 削除機能実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -32,6 +32,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
    def list_params
      params.require(:list).permit(:title, :body, :imege)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => "homes#top"
-  post 'lists'=> 'lists#create' #書き換え忘れるとエラー起こす
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  get 'top' => 'home#top'
+  resources :lists
+
 end
